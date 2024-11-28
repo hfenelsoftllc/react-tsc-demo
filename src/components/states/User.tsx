4b75a9e552ea4f42f8a1843774e562158ed74e43
Fenel Hyacinthe <hfenelsoftllc@gmail.com>
2024-11-28 07:13:31 -0500

    add discriminated union concept

--- src/components/states/User.tsx
+++ src/components/states/User.tsx
@@ -8,7 +8,7 @@ type AuthUser ={
 
 export const User =() =>{
     // Deconstruction of useState props
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin= () =>{
         setUser({
@@ -17,16 +17,16 @@ export const User =() =>{
         })
     }
 
-    const handleLogout = () =>{
-        setUser(null)
-    }
+    // const handleLogout = () =>{
+    //     setUser(null)
+    // }
 
     return(
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
